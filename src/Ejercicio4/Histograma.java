c1a767db1a86a57f67af07db9436b7d4a3dc3ba3
JoseLuisPinzon8 <jlpinzonr@unal.edu.co>
2017-03-08 23:56:55 -0500

    Hola profe, aca va el punto 4. Para evaluarlo con distintos numeros cambie el ciclo hasta el que llega el for de la clase histograma.

--- src/Ejercicio4/Histograma.java
+++ src/Ejercicio4/Histograma.java
@@ -20,27 +20,22 @@ public class Histograma {
         ejercicio4 karel= new ejercicio4();
         
         Thing pos1[]= new Thing[10];
-        Thing pos2[]= new Thing[100];
-        Thing pos3[]= new Thing[100];
-        Thing pos4[]= new Thing[100];
+        Thing pos2[]= new Thing[10];
+        Thing pos3[]= new Thing[10];
+        Thing pos4[]= new Thing[10];
         
-        for(int i=0; i<5; i++){    
+        for(int i=0; i<=6; i++){    
         pos1[i] = new Thing(karel.getA(), 1, 1);
         }
-        for(int i=0; i<4; i++){    
+        for(int i=0; i<=3; i++){    
         pos2[i] = new Thing(karel.getA(), 2, 1);
         }
-        for(int i=0; i<2; i++){    
+        for(int i=0; i<=4; i++){    
         pos3[i] = new Thing(karel.getA(), 3, 1);
         }
-        for(int i=0; i<5; i++){    
+        for(int i=0; i<=2; i++){    
         pos4[i] = new Thing(karel.getA(), 4, 1);
         }
         karel.jugar();
-
-        
-        
-                
-
     }
 }
